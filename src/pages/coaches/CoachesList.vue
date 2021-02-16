20db6cdf89400e87dddd04708d0b91245153f67b
Gaurav-dawadi <gaurabdawadi@gamil.com>
2021-02-16 12:59:42 +0545

    feat/style: add new coachitem component and style coachlist

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,12 +5,16 @@
             <router-link to='register'>Register as Coach</router-link>
         </div>
         <ul v-if="hasCoaches">
-            <li 
+            <coach-item 
                 v-for='coach in filteredCoaches'
                 :key='coach.id'
+                :id='coach.id'
+                :firstName="coach.firstName"
+                :lastName="coach.lastName"
+                :rate='coach.hourlyRate'
+                :areas="coach.areas"
             >
-                {{coach.firstName}}
-            </li>
+            </coach-item>
         </ul>
         <h3 v-else>No coaches Found</h3>
     </section>
@@ -18,7 +22,12 @@
 
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue'
+
 export default {
+    components:{
+        CoachItem,
+    },
     computed: {
         filteredCoaches(){
             return this.$store.getters['coaches/coaches']
@@ -31,5 +40,14 @@ export default {
 </script>
 
 <style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
 
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
